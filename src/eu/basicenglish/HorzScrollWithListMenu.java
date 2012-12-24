6147456f7d1fe4eaa3e4db45e9a99b50f0c6b070
Artur Gurgul <artur.gurgul@gmail.com>
2012-12-24 18:56:04 +0100

    init

---
+++ src/eu/basicenglish/HorzScrollWithListMenu.java
@@ -0,0 +1,127 @@
+
+package eu.basicenglish;
+
+import eu.basicenglish.MyHorizontalScrollView.SizeCallback;
+import eu.basicenglish.R;
+
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class HorzScrollWithListMenu extends Activity {
+    MyHorizontalScrollView scrollView;
+    View menu;
+    View app;
+    ImageView btnSlide;
+    boolean menuOut = false;
+    Handler handler = new Handler();
+    int btnWidth;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        scrollView = (MyHorizontalScrollView) inflater.inflate(R.layout.horz_scroll_with_list_menu, null);
+        setContentView(scrollView);
+
+        menu = inflater.inflate(R.layout.horz_scroll_menu, null);
+        app = inflater.inflate(R.layout.horz_scroll_app, null);
+        ViewGroup tabBar = (ViewGroup) app.findViewById(R.id.tabBar);
+
+        ListView listView = (ListView) app.findViewById(R.id.list);
+        ViewUtils.initListView(this, listView, "Item ", 30, android.R.layout.simple_list_item_1);
+
+        listView = (ListView) menu.findViewById(R.id.list);
+        ViewUtils.initListView(this, listView, "Menu ", 30, android.R.layout.simple_list_item_1);
+
+        btnSlide = (ImageView) tabBar.findViewById(R.id.BtnSlide);
+        btnSlide.setOnClickListener(new ClickListenerForScrolling(scrollView, menu));
+
+        final View[] children = new View[] { menu, app };
+
+        // Scroll to app (view[1]) when layout finished.
+        int scrollToViewIdx = 1;
+        scrollView.initViews(children, scrollToViewIdx, new SizeCallbackForMenu(btnSlide));
+    }
+
+    /**
+     * Helper for examples with a HSV that should be scrolled by a menu View's width.
+     */
+    static class ClickListenerForScrolling implements OnClickListener {
+        HorizontalScrollView scrollView;
+        View menu;
+        /**
+         * Menu must NOT be out/shown to start with.
+         */
+        boolean menuOut = false;
+
+        public ClickListenerForScrolling(HorizontalScrollView scrollView, View menu) {
+            super();
+            this.scrollView = scrollView;
+            this.menu = menu;
+        }
+
+        public void onClick(View v) {
+            Context context = menu.getContext();
+            String msg = "Slide " + new Date();
+            Toast.makeText(context, msg, 1000).show();
+            System.out.println(msg);
+
+            int menuWidth = menu.getMeasuredWidth();
+
+            // Ensure menu is visible
+            menu.setVisibility(View.VISIBLE);
+
+            if (!menuOut) {
+                // Scroll to 0 to reveal menu
+                int left = 0;
+                scrollView.smoothScrollTo(left, 0);
+            } else {
+                // Scroll to menuWidth so menu isn't on screen.
+                int left = menuWidth;
+                scrollView.smoothScrollTo(left, 0);
+            }
+            menuOut = !menuOut;
+        }
+    }
+
+    /**
+     * Helper that remembers the width of the 'slide' button, so that the 'slide' button remains in view, even when the menu is
+     * showing.
+     */
+    static class SizeCallbackForMenu implements SizeCallback {
+        int btnWidth;
+        View btnSlide;
+
+        public SizeCallbackForMenu(View btnSlide) {
+            super();
+            this.btnSlide = btnSlide;
+        }
+
+        public void onGlobalLayout() {
+            btnWidth = btnSlide.getMeasuredWidth();
+            System.out.println("btnWidth=" + btnWidth);
+        }
+
+        public void getViewSize(int idx, int w, int h, int[] dims) {
+            dims[0] = w;
+            dims[1] = h;
+            final int menuIdx = 0;
+            if (idx == menuIdx) {
+                dims[0] = w - btnWidth;
+            }
+        }
+    }
+}
---
+++ src/eu/basicenglish/MyHorizontalScrollView.java
@@ -0,0 +1,108 @@
+
+package eu.basicenglish;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.widget.HorizontalScrollView;
+
+public class MyHorizontalScrollView extends HorizontalScrollView {
+    public MyHorizontalScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    public MyHorizontalScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public MyHorizontalScrollView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    void init(Context context) {
+        setHorizontalFadingEdgeEnabled(false);
+        setVerticalFadingEdgeEnabled(false);
+    }
+
+    public void initViews(View[] children, int scrollToViewIdx, SizeCallback sizeCallback) {
+        
+    	ViewGroup parent = (ViewGroup) getChildAt(0);
+
+        for (int i = 0; i < children.length; i++) {
+            children[i].setVisibility(View.INVISIBLE);
+            parent.addView(children[i]);
+        }
+        
+        OnGlobalLayoutListener listener = new MyOnGlobalLayoutListener(parent, children, scrollToViewIdx, sizeCallback);
+        getViewTreeObserver().addOnGlobalLayoutListener(listener);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    class MyOnGlobalLayoutListener implements OnGlobalLayoutListener {
+        ViewGroup parent;
+        View[] children;
+        int scrollToViewIdx;
+        int scrollToViewPos = 0;
+        SizeCallback sizeCallback;
+
+        public MyOnGlobalLayoutListener(ViewGroup parent, View[] children, int scrollToViewIdx, SizeCallback sizeCallback) {
+            this.parent = parent;
+            this.children = children;
+            this.scrollToViewIdx = scrollToViewIdx;
+            this.sizeCallback = sizeCallback;
+        }
+
+        public void onGlobalLayout() {
+
+            final HorizontalScrollView me = MyHorizontalScrollView.this;
+
+            me.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+            sizeCallback.onGlobalLayout();
+
+            parent.removeViewsInLayout(0, children.length);
+
+            final int w = me.getMeasuredWidth();
+            final int h = me.getMeasuredHeight();
+
+            int[] dims = new int[2];
+            scrollToViewPos = 0;
+            for (int i = 0; i < children.length; i++) {
+                sizeCallback.getViewSize(i, w, h, dims);
+                children[i].setVisibility(View.VISIBLE);
+                parent.addView(children[i], dims[0], dims[1]);
+                if (i < scrollToViewIdx) {
+                    scrollToViewPos += dims[0];
+                }
+            }
+
+            new Handler().post(new Runnable() {
+                
+                public void run() {
+                    me.scrollBy(scrollToViewPos, 0);
+                }
+            });
+        }
+    }
+    
+    public interface SizeCallback {
+        
+        public void onGlobalLayout();
+        public void getViewSize(int idx, int w, int h, int[] dims);
+    }
+}
